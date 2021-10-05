919d1820b45972b9aa7f1aefda97f6eeb20006fd
ritik-cyber <ritikdhingra03@gmail.com>
2021-10-05 17:51:49 +0530

    done

--- block-BJaaiq/code/index.js
+++ block-BJaaiq/code/index.js
@@ -1,13 +1,27 @@
-let quotes = document.querySelector('quotes')
+let quotesElm = document.querySelector(".quotes");
+let counter = 0;
 
+let limit = 1;
+let current = 0;
 
-handleScroll(){
-    let li = document.createElement('li')
-    let div = document.createElement('div')
-    let h1 = document.createElement('h1')
-    h1.innerText= quotes.quoteText
-    let p = document.createElement('p')
+function handleScroll() {
+  if (current > limit) addQuote(1);
+  else current++;
 }
 
+function addQuote(n) {
+  for (let i = counter; i < counter + n; i++) {
+    let li = document.createElement("li");
+    let h1 = document.createElement("h1");
+    h1.innerText = quotes[i].quoteText;
+    let p = document.createElement("p");
+    p.innerText = quotes[i].quoteAuthor;
+    li.append(h1, p);
+    quotesElm.append(li);
+  }
+  if (counter === quotesElm.length) counter = 0;
+  else counter += n;
+}
 
-quotes.addEventListener("scroll", handleScroll)
+window.addEventListener("load", () => addQuote(15));
+window.addEventListener("scroll", handleScroll);
