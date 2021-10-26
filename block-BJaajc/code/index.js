618d0a6ebf35168dac8fdd4a03d37e1323f88234
ritik-cyber <ritikdhingra03@gmail.com>
2021-10-26 14:38:41 +0530

    doNe againi

--- block-BJaajc/code/index.js
+++ block-BJaajc/code/index.js
@@ -2,4 +2,20 @@ function fullName(firstName, lastName) {
   return firstName + " " + lastName;
 }
 
-module.exports = fullName;
+function palindrome(str) {
+  var len = str.length;
+  var mid = Math.floor(len / 2);
+
+  for (var i = 0; i < mid; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+module.exports = {
+  fullName,
+  palindrome,
+};
--- block-BJaajc/code/index.test.js
+++ block-BJaajc/code/index.test.js
@@ -1,5 +1,9 @@
-const fullName = require("./index");
+const index = require("./index");
 
 test(`fullName 'ritik' 'dhingra' is to equal 'ritik dhingra'`, () => {
-  expect(fullName("ritik", "dhingra")).toBe("ritik dhingra");
+  expect(index.fullName("ritik", "dhingra")).toBe("ritik dhingra");
+});
+
+test(`palindrome 'madam' is to equal 'true'`, () => {
+  expect(index.palindrome("madam")).toBe(true);
 });
